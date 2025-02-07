b16ced5490c262ec802e798927eec3e09a5383d7
Birajsitaula <birajsitaula7@gmail.com>
2025-02-07 15:39:45 +0545

    adding the userModel

---
+++ models/userModel.ts
@@ -0,0 +1,29 @@
+import { DataType, DataTypes } from "sequelize";
+import connection from "./index";
+
+export default connection.define("Kandara",
+    {
+        id:
+        {
+            type:DataTypes.STRING,
+            autoIncrement:true,
+            primaryKey:true,
+        },
+        email:
+        {
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true
+        },
+        password:
+        {
+            type:DataTypes.STRING,
+            allowNull:false
+        }
+        
+    },
+    {
+        timestamps:false,
+        freezeTableName:true
+    }
+)
