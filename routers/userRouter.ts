0c559b62daf4c9e48c9978fbdc467d180d0f2a17
Birajsitaula <birajsitaula7@gmail.com>
2025-02-07 16:22:06 +0545

    update the UserRouter

---
+++ routers/userRouter.ts
@@ -0,0 +1,35 @@
+import userModel from "../models/userModel";
+import { Router, Request, Response } from "express";
+import bcrypt from "bcrypt";
+
+const router = Router();
+
+router.post("/name",async(req:Request , res:Response)=>
+{
+    const {email, password} = req.body;
+    try{
+        if(!email || !password)
+        {
+             res.status(501).json({message:"Please enter the email and the password "});
+             return;
+        }
+        // const existingUser = await userModel.findOne({email})
+    //    if(existingUser)
+    //    {
+    //     res.status(400).json({message:"User already existing "})
+    //     return;;
+    //    }
+
+    const saltRounds = 10 ;
+    const hashPassword :any = await bcrypt.hash(password,saltRounds)
+    const newUser = await  userModel.create(email, hashPassword);
+    res.status(200).json({message:"user update successfully",newUser})
+
+    }
+    catch(err)
+    {
+        console.error("There is an error while add the user in the database ",err);
+        res.status(500).json({message:"There is an error while add the user in the database "})
+    }
+
+})
