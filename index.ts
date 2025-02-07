25295a104138405fa78b7d073aa34c8dcf0fcf3b
Birajsitaula <birajsitaula7@gmail.com>
2025-02-07 15:20:54 +0545

    initial update

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import "dotenv/config";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.listen(8000,()=>
+{
+    console.log("The server is running successfully ");
+})
