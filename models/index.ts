5246c720d900d0a4f9c7b567c2fb7c61d59895e7
Birajsitaula <birajsitaula7@gmail.com>
2025-02-07 15:32:24 +0545

    updating the index.ts

--- models/index.ts
+++ models/index.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 const {dbName, dbUsername, dbPassword,dbHost}= process.env
 const sequelize = new Sequelize(dbName, dbUsername, dbPassword, {
     host: dbHost,
-    dialect:"mysql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+    dialect:"mysql" 
   });
 
   export default sequelize;
