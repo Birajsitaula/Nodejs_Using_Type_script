02786f77cb29db3b5132fa43ec043c5da3fb2dad
Birajsitaula <birajsitaula7@gmail.com>
2025-02-07 15:27:20 +0545

    update in the code

--- index.ts
+++ index.ts
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-
-app.listen(8000,()=>
+const port = process.env.PORT
+app.listen(port || 8001,()=>
 {
     console.log("The server is running successfully ");
 })
