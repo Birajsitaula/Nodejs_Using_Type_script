e782807d4eb24f8566ae45d4eee33647f1fd2c57
Birajsitaula <birajsitaula7@gmail.com>
2025-02-08 11:36:06 +0545

    change but not work code

--- routers/userRouter.ts
+++ routers/userRouter.ts
@@ -1,6 +1,6 @@
 import userModel from "../models/userModel";
 import { Router, Request, Response } from "express";
-import bcrypt from "bcrypt";
+import * as bcrypt from 'bcrypt';
 
 const router = Router();
 
@@ -13,20 +13,20 @@ router.post("/add",async(req:Request , res:Response)=>
              res.status(501).json({message:"Please enter the email and the password "});
              return;
         }
-        // const existingUser = await userModel.findOne({email})
+    //     const existingUser = await userModel.findOne({email})
     //    if(existingUser)
     //    {
     //     res.status(400).json({message:"User already existing "})
     //     return;;
     //    }
 
-    // const saltRounds = 10 ;
-    // const hashPassword :any = await bcrypt.hash(password,saltRounds)
-    // const newUser = await  userModel.create({email, password: hashPassword});
-    // res.status(200).json({message:"user update successfully",newUser})
+    const saltRounds = 10 ;
+    const hashPassword :any = await bcrypt.hash(password,saltRounds)
+    const newUser = await  userModel.create({email, password: hashPassword});
+    res.status(200).json({message:"user update successfully",newUser})
 
-    const newUser = await userModel.create({email,password});    //  ==> without using the bcrypt 
-    res.status(201).json({message :"user updated successfully ",newUser}) // ==> without using the bcrypt 
+    // const newUser = await userModel.create({email,password});    //  ==> without using the bcrypt 
+    // res.status(201).json({message :"user updated successfully ",newUser}) // ==> without using the bcrypt 
     }
     catch(err)
     {
